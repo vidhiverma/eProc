89fd456665674a3147eb5a50eb13248a6aba5f8f
vikas.kiroula <vikas.kiroula@ZIPLBLRSY139.zycus.net>
2018-06-18 15:41:27 +0530

    search

--- Scripts/CommonLibraries/Search/Script1528970046681.groovy
+++ Scripts/CommonLibraries/Search/Script1528970046681.groovy
@@ -22,7 +22,7 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.callTestCase(findTestCase('CommonLibraries/Login'), [:], FailureHandling.STOP_ON_FAILURE)
 
-WebUI.waitForPageLoad(10)
+WebUI.waitForPageLoad(5)
 
 WebUI.sendKeys(findTestObject('OnlineStorePage/SerachBox'), findTestData('TestData').getValue(2, 4))
 
